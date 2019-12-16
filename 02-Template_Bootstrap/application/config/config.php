0cd32369904ffbae2b774199d765717599f5db8e
neto <jsn.ads@gmail.com>
2019-12-16 00:09:55 -0300

    modificado o htaccess e adicionado script.js

--- 02-Template_Bootstrap/application/config/config.php
+++ 02-Template_Bootstrap/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/JOB/PROJETOSWEB/CodeIgniter/02-Template_Bootstrap/';
+$config['base_url'] = 'http://localhost/PROJETOS/04-CODEIGNITER/02-Template_Bootstrap/';
 
 /*
 |--------------------------------------------------------------------------
